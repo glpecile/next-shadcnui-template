887e6e8b2994924b0eb4c45a1b740c8430461648
Gian <58370608+glpecile@users.noreply.github.com>
2025-06-16 17:23:54 -0300

    chore(deps): bump tailwind css with upgrade script and tailwind merge

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
 					CSS, and Motion (fka Framer Motion).
 				</p>
 				<div className="z-10 w-full items-center justify-between text-sm lg:flex">
-					<p className="fixed top-0 left-0 flex w-full justify-center border-gray-300 border-b bg-gradient-to-b from-zinc-200 pt-8 pb-6 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30">
+					<p className="fixed top-0 left-0 flex w-full justify-center border-gray-300 border-b bg-linear-to-b from-zinc-200 pt-8 pb-6 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30">
 						Get started by editing&nbsp;
 						<code className="font-bold font-mono">
 							app/page.tsx
 						</code>
 					</p>
-					<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
+					<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-linear-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
 						<a
 							className="flex place-items-center gap-2 p-8 hover:underline"
 							href="https://glpecile.xyz"
